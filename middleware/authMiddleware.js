e7169d6fd636ddd2eeb801fe353f72e12424a9cc
joeshwoa <joeshwoa.george@gmail.com>
2024-11-04 15:33:36 +0200

    fix middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const token = req.header("Authorization");
+  const token = req.header("Authorization").split(' ')[1];
   if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
 
   try {
+    console.log(token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
     req.user = decoded.user;
     next();
   } catch (err) {
+    console.log(err);
     res.status(401).json({ msg: "Token is not valid" });
   }
 };
